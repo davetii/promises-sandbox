9f37b2c4d39ed098b2a37362bebdaa006987c4de
Dave Turner <davetii@gmail.com>
2019-11-08 23:18:22 -0500

    Adding more test examples

--- test/sqroot-test.js
+++ test/sqroot-test.js
@@ -4,8 +4,31 @@ const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-describe('test-square-ropot', function() {
+describe('test-square-root', function() {
+    it('square root to be fulfilled', function() {
+        // this.timeout(6000); use this to solve timeout problems
+        return expect(sq.sq(2)).to.be.fulfilled;
+    });
+
     it('square root of 2 is 4', function() {
         return expect(sq.sq(2)).to.eventually.be.equal(4);
     });
-})
+
+    it('square root should fail when not given a number', function() {
+        return expect(sq.sq('w')).to.be.rejected;
+    });
+});
+
+describe('test-square-root-with-done-callback', function() {
+    it('square root to be fulfilled', function(done) {
+        expect(sq.sq(2)).to.be.fulfilled.notify(done);
+    });
+
+    it('square root of 2 is 4', function(done) {
+        expect(sq.sq(2)).to.eventually.be.equal(4).notify(done);
+    });
+
+    it('square root should fail when not given a number', function(done) {
+        expect(sq.sq('w')).to.be.rejected.notify(done);
+    });
+});
